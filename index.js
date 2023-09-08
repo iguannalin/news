55b463a00ac1bd9754cd3e2a45987e146e76eddc
seasons986 <annalin12345@gmail.com>
2023-09-08 01:00:18 -0400

    cleanup

--- index.js
+++ index.js
@@ -5,21 +5,9 @@ window.addEventListener("load", () => {
     return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
   }
 
-  // const poem = "You may write me down in history\nWith your bitter, twisted lies,\nYou may trod me in the very dirt\nBut still, like dust, I'll rise.\n\nDoes my sassiness upset you?\nWhy are you beset with gloom?\n’Cause I walk like I've got oil wells\nPumping in my living room.\n\nJust like moons and like suns,\nWith the certainty of tides,\nJust like hopes springing high,\nStill I'll rise.\n";
-  // const input = document.getElementById("input")
-  // input.onchange =backtrack;
-
   const container = document.getElementById("container");
 
   function backtrack(poem, reverse = false) {
-    // poem = input.value;
-    // poem.split("\n").forEach((line) => {
-    //   container.innerHTML += (line + "<br>");
-    // });
-    // poem.split("\n").reverse().forEach((line) => {
-    //   container.innerHTML += (line + "<br>");
-    // });
-    console.log(poem);
     container.innerHTML += "<br>";
     if (reverse) poem.split(" ").forEach((word) => {
       container.innerHTML += (word + " ");
