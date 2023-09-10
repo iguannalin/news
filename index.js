28a2d145fb7720c20432e7b3d80c9a26bd534f11
seasons986 <annalin12345@gmail.com>
2023-09-10 12:31:16 -0400

    more cohesive poem

--- index.js
+++ index.js
@@ -7,34 +7,55 @@ window.addEventListener("load", () => {
 
   const container = document.getElementById("container");
   const lines = [];
+  let time = 0;
+  let interval = () => getRandomInt(1, 50);
+
+  function typewrite(word, breakline = false) {
+    if (breakline) word.split(" ").forEach((ww) => { 
+        ww.split("").forEach((ch) => {
+        setTimeout(() => { 
+          if(ch == "*") container.innerHTML += " ";
+          else container.innerHTML += ch;
+        }, time += interval());
+      });
+    });
+    else word.split("").forEach((ch) => {
+      setTimeout(() => { 
+        if (ch == "*") container.innerHTML += " ";
+        else container.innerHTML += ch;
+      }, time += interval());
+    });
+  }
 
   function fronttrack(poem) {
     container.innerHTML += "<br>";
     let line = "";
-    poem = poem.split(" ").forEach((word) => {
+    poem.split(" ").forEach((word) => {
       if (word.match(/[.,-]/g)) return;
-      container.innerHTML += (word + " ");
-      line += (word + " ");
+      typewrite((word+"*"));
+      line += (word + "*");
       if (Math.random() > 0.85) {
         container.innerHTML += "<br>";
         lines.push(line);
         line = "";
       }
     });
+    lines.push(line);
+    backtrack();
   }
 
-  function backtrack(poem) {
+  function backtrack() {
     lines.reverse().forEach((line) => {
-      container.innerHTML += (line + "<br>");
+      typewrite((line), true);
     });    
   }
 
   fetch("https://api.nytimes.com/svc/news/v3/content/all/all.json?api-key=AnMEmLbge9xojcPa7zOuToDvvpNlr7iK").then((r) => r.json()).then((result) => {
     let poem = "";  
-    for (let i = 0; i < getRandomInt(1, 3); i++) {
-      poem += result.results[getRandomInt(0, result.results.length)].abstract;
+    for (let i = 0; i < getRandomInt(1, 2); i++) {
+      let article = result.results[getRandomInt(0, result.results.length)].abstract;
+      poem += article.substring(0, getRandomInt(3, article.length));
     }
     fronttrack(poem);
-    backtrack(poem);
   });
 });
