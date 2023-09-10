b973a2e507eaa6ab74ee0ef3e560ef682838dfde
seasons986 <annalin12345@gmail.com>
2023-09-10 11:59:25 -0400

    styling

--- index.js
+++ index.js
@@ -6,17 +6,27 @@ window.addEventListener("load", () => {
   }
 
   const container = document.getElementById("container");
+  const lines = [];
 
-  function backtrack(poem, reverse = false) {
+  function fronttrack(poem) {
     container.innerHTML += "<br>";
-    if (reverse) poem.split(" ").forEach((word) => {
+    let line = "";
+    poem = poem.split(" ").forEach((word) => {
+      if (word.match(/[.,-]/g)) return;
       container.innerHTML += (word + " ");
-      if (Math.random() > 0.85) container.innerHTML += "<br>";
+      line += (word + " ");
+      if (Math.random() > 0.85) {
+        container.innerHTML += "<br>";
+        lines.push(line);
+        line = "";
+      }
     });
-    else poem.split(" ").reverse().forEach((word) => {
-      container.innerHTML += (word + " ");
-      if (Math.random() > 0.85) container.innerHTML += "<br>";
-    });
+  }
+
+  function backtrack(poem) {
+    lines.reverse().forEach((line) => {
+      container.innerHTML += (line + "<br>");
+    });    
   }
 
   fetch("https://api.nytimes.com/svc/news/v3/content/all/all.json?api-key=AnMEmLbge9xojcPa7zOuToDvvpNlr7iK").then((r) => r.json()).then((result) => {
@@ -24,7 +34,7 @@ window.addEventListener("load", () => {
     for (let i = 0; i < getRandomInt(1, 3); i++) {
       poem += result.results[getRandomInt(0, result.results.length)].abstract;
     }
-    backtrack(poem, true);
+    fronttrack(poem);
     backtrack(poem);
   });
 });
